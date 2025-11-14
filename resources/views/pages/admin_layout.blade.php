6dc7f185ba4b8e444b3a446ae949fadb5604ebd6
JasNguyn <ponnguyen0108@gmail.com>
2025-11-14 19:29:44 +0700

    avt + fullname admin

--- resources/views/pages/admin_layout.blade.php
+++ resources/views/pages/admin_layout.blade.php
@@ -73,7 +73,7 @@ body {
         <li class="dropdown">
             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                 <img alt="" src="{{asset('backend/images/2.png')}}">
-                <span class="username">Admin</span>
+				<span class="username">{{ Session::get('admin_name') }}</span>
                 <b class="caret"></b>
             </a>
             <ul class="dropdown-menu extended logout">
