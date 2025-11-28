931a54cb6594ee68a7d90a7cc51333f1df060739
tuilakhoa2512 <fbkhoqkhoa2512@gmail.com>
2025-11-28 15:23:48 +0700

    Fix Google login to use env variables

--- config/services.php
+++ config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+
+    'google' => [
+        'client_id' => env('GOOGLE__CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
+    ],
+
 ];
